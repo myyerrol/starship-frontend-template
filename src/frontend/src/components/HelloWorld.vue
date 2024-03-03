98a691c09f47ade7c3446477c0de297da5cded5a
myyerrol <myyerrol@126.com>
2024-03-03 10:15:48 +0800

    feat: add element-plus framework

--- src/frontend/src/components/HelloWorld.vue
+++ src/frontend/src/components/HelloWorld.vue
@@ -11,6 +11,7 @@
 
     <div class="card">
         <button type="button" @click="count++">count is {{ count }}</button>
+        <el-button type="primary">我是 ElButton</el-button>
         <p>
             Edit
             <code>components/HelloWorld.vue</code> to test HMR
